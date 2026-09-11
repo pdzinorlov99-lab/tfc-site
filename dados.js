a30924bc8fe67ada97fa19d637ac4ffb5bcdc4f2
pdzinorlov99-lab <manchestermadrid16@gmail.com>
2026-09-11 14:15:44 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,71 @@
+const TFC_DATA = {
+
+    p4p: [
+        ["Gabriel Pardal", "Peso Pesado", "7-0-0", "7 KO"],
+        ["Eren Serelepe", "Peso Meio-Médio", "9-0-0", "9 KO"],
+        ["Rou Spyder", "Peso Leve", "7-0-0", "6 KO"],
+        ["Nox Spyder", "Peso Médio", "12-2-0", "7 KO"],
+        ["Xabinho Oliveira", "Peso Leve", "7-0-0", "6 KO"],
+        ["Jaguar Serelepe", "Peso Meio-Médio", "11-5-0", "9 KO"],
+        ["Kauezin", "Peso Pesado", "5-0-0", "5 KO"],
+        ["Jin Ankalaev", "Peso Médio", "7-1-0", "4 KO"],
+        ["Xavier McGregor", "Peso Médio", "9-3-0", "7 KO"],
+        ["Sand Oliveira", "Peso Leve", "10-4-0", "6 KO"]
+    ],
+
+    "Peso Leve": [
+        ["C", "Rou Spyder", "7-0-0", "6 KO"],
+        ["1", "Kevyn Bulldog", "8-4-0", "2 KO"],
+        ["2", "Sand Oliveira", "10-4-0", "6 KO"],
+        ["3", "Xabinho Oliveira", "7-0-0", "6 KO"],
+        ["4", "Gabriel Eastwood", "9-6-0", "8 KO"],
+        ["5", "Ryan Mishimoto", "4-2-0", "3 KO"],
+        ["6", "Lucas Maia", "3-0-0", "1 KO"],
+        ["7", "Ezequiel Lopes", "6-3-0", "3 KO"],
+        ["8", "Jorge Mishimoto", "5-1-0", "4 KO"],
+        ["9", "Justin Gaethje", "3-0-0", "3 KO"],
+        ["10", "Gabriel Oliveira", "3-0-0", "2 KO"]
+    ],
+
+    "Peso Meio-Médio": [
+        ["C", "Eren Serelepe", "9-0-0", "9 KO"],
+        ["1", "Jaguar Serelepe", "11-5-0", "9 KO"],
+        ["2", "Takeshi Mishimoto", "5-1-0", "2 KO"],
+        ["3", "Daniel Santos", "8-5-1", "7 KO"],
+        ["4", "Kaio Buffer", "5-3-1", "2 KO"],
+        ["5", "Campos Bulldog", "4-1-0", "4 KO"],
+        ["6", "Pdzin Orlov", "5-4-0", "4 KO"],
+        ["7", "Maurício Nascimento", "5-3-0", "3 KO"],
+        ["8", "Noca Leitinho Volk", "5-4-1", "4 KO"],
+        ["9", "Dante Serelepe", "3-0-0", "3 KO"],
+        ["10", "Aleksandr Tsarukyan", "2-3-0", "2 KO"]
+    ],
+
+    "Peso Médio": [
+        ["C", "Nox Spyder", "12-2-0", "7 KO"],
+        ["1", "Jin Ankalaev", "7-1-0", "4 KO"],
+        ["2", "Xavier McGregor", "9-3-0", "7 KO"],
+        ["3", "Juan Diaz", "8-3-2", "6 KO"],
+        ["4", "Pzz", "2-0-0", "1 KO"],
+        ["5", "Felipe Plata", "3-1-1", "3 KO"],
+        ["6", "Alexandre Rodrigues", "4-0-0", "4 KO"],
+        ["7", "Davi Lopes", "4-0-0", "0 KO"],
+        ["8", "Israel Adesanya", "4-0-0", "3 KO"],
+        ["9", "Rodrigo Garro", "4-2-0", "3 KO"],
+        ["10", "Glover Pereira", "3-0-0", "1 KO"]
+    ],
+
+    "Peso Pesado": [
+        ["C", "Gabriel Pardal", "7-0-0", "7 KO"],
+        ["1", "Asgard Gregor", "4-0-0", "4 KO"],
+        ["2", "Felipe Rodrigues", "7-3-0", "6 KO"],
+        ["3", "Kauezin", "5-0-0", "5 KO"],
+        ["4", "Rafael Moncayo", "7-2-0", "4 KO"],
+        ["5", "Ramiro Lopes", "3-0-0", "2 KO"],
+        ["6", "Raphael Santos", "4-1-0", "4 KO"],
+        ["7", "Conor Jhon", "4-1-0", "3 KO"],
+        ["8", "Gabriel Mishimoto", "5-4-1", "3 KO"],
+        ["9", "Ryan Mitchell", "4-0-0", "2 KO"],
+        ["10", "Rafael Bini", "3-1-0", "3 KO"]
+    ]
+};
